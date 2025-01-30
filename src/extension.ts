0b610eb1ff9464710ec0cbe69a8ff5c0168de700
JBL <0upbeats-daffier@icloud.com>
2025-01-30 19:47:46 +0800

    ⚙️chore(package.json) update model to llama-3.3-70b-specdec ⚙️chore(src/extension.ts) refactor suggest function

--- src/extension.ts
+++ src/extension.ts
@@ -18,34 +18,34 @@ import { TextDecoder } from "util";
  * @param context The extension context.
  */
 export function activate(context: vscode.ExtensionContext) {
-	let cancellationTokenSource: vscode.CancellationTokenSource | null = null;
+  let cancellationTokenSource: vscode.CancellationTokenSource | null = null;
 
-	let disposable = vscode.commands.registerCommand(
-		"chadcommit.suggest",
-		async () => {
-			if (cancellationTokenSource) {
-				vscode.window
-					.showInformationMessage("Thinking...", "Cancel")
-					.then((selectedItem) => {
-						if (selectedItem === "Cancel") {
-							cancellationTokenSource?.cancel();
-							cancellationTokenSource?.dispose();
-							cancellationTokenSource = null;
-						}
-					});
-				return;
-			} else {
-				cancellationTokenSource = new vscode.CancellationTokenSource();
-			}
+  let disposable = vscode.commands.registerCommand(
+    "chadcommit.suggest",
+    async () => {
+      if (cancellationTokenSource) {
+        vscode.window
+          .showInformationMessage("Thinking...", "Cancel")
+          .then((selectedItem) => {
+            if (selectedItem === "Cancel") {
+              cancellationTokenSource?.cancel();
+              cancellationTokenSource?.dispose();
+              cancellationTokenSource = null;
+            }
+          });
+        return;
+      } else {
+        cancellationTokenSource = new vscode.CancellationTokenSource();
+      }
 
-			await suggest(cancellationTokenSource.token);
+      await suggest(cancellationTokenSource.token);
 
-			cancellationTokenSource.dispose();
-			cancellationTokenSource = null;
-		},
-	);
+      cancellationTokenSource.dispose();
+      cancellationTokenSource = null;
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
@@ -58,123 +58,122 @@ export function deactivate() {}
  * @param cancelToken The cancellation token to cancel the request.
  */
 const suggest = async (cancelToken: vscode.CancellationToken) => {
-	try {
-		const config = vscode.workspace.getConfiguration("chadcommit");
+  try {
+    const config = vscode.workspace.getConfiguration("chadcommit");
 
-		const openAiKey: string | undefined = config.get("openAiKey");
-		const prompt: string | undefined = config.get("prompt");
-		const model: string | undefined = config.get("model");
+    const openAiKey: string | undefined = config.get("openAiKey");
+    const prompt: string | undefined = config.get("prompt");
+    const model: string | undefined = config.get("model");
 
-		if (!openAiKey) {
-			const action = "Go to Settings";
+    if (!openAiKey) {
+      const action = "Go to Settings";
 
-			vscode.window
-				.showInformationMessage("Set your OpenAI API key here first!", action)
-				.then((selectedItem) => {
-					if (selectedItem === action) {
-						vscode.commands.executeCommand(
-							"workbench.action.openSettings",
-							"chadcommit.openAiKey",
-						);
-					}
-				});
-			return;
-		}
+      vscode.window
+        .showInformationMessage("Set your OpenAI API key here first!", action)
+        .then((selectedItem) => {
+          if (selectedItem === action) {
+            vscode.commands.executeCommand(
+              "workbench.action.openSettings",
+              "chadcommit.openAiKey"
+            );
+          }
+        });
+      return;
+    }
 
-		const gitExtension = vscode.extensions.getExtension("vscode.git");
+    const gitExtension = vscode.extensions.getExtension("vscode.git");
 
-		if (!gitExtension) {
-			vscode.window.showErrorMessage("Failed to find the Git extension!");
-			return;
-		}
+    if (!gitExtension) {
+      vscode.window.showErrorMessage("Failed to find the Git extension!");
+      return;
+    }
 
-		const git = gitExtension.exports.getAPI(1);
+    const git = gitExtension.exports.getAPI(1);
 
-		const currentRepo = git.repositories[0];
+    const currentRepo = git.repositories[0];
 
-		if (!currentRepo) {
-			vscode.window.showErrorMessage("Failed to find a Git repository!");
-			return;
-		}
+    if (!currentRepo) {
+      vscode.window.showErrorMessage("Failed to find a Git repository!");
+      return;
+    }
 
-		const stagedChangesDiff = await currentRepo.diffIndexWith("HEAD");
+    const stagedChangesDiff = await currentRepo.diffIndexWith("HEAD");
 
-		if (stagedChangesDiff.length === 0) {
-			vscode.window.showErrorMessage("There is no staged changes!");
-			return;
-		}
+    if (stagedChangesDiff.length === 0) {
+      vscode.window.showErrorMessage("There is no staged changes!");
+      return;
+    }
 
-		let parsed = [],
-			deleted = [],
-			renamed = [];
+    let parsed = [],
+      deleted = [],
+      renamed = [];
 
-		for (const change of stagedChangesDiff) {
-			switch (change.status) {
-				case 3:
-					renamed.push(
-						`RENAMED: ${change.originalUri.path} to ${change.renameUri.path};`,
-					);
-					break;
-				case 6:
-					deleted.push(`DELETED: ${change.originalUri.path};`);
-					break;
-				default:
-					const fileDiff = await currentRepo.diffIndexWithHEAD(
-						change.uri.fsPath,
-					);
-					parsed.push(fileDiff);
-					break;
-			}
-		}
+    for (const change of stagedChangesDiff) {
+      switch (change.status) {
+        case 3:
+          renamed.push(
+            `RENAMED: ${change.originalUri.path} to ${change.renameUri.path};`
+          );
+          break;
+        case 6:
+          deleted.push(`DELETED: ${change.originalUri.path};`);
+          break;
+        default:
+          const fileDiff = await currentRepo.diffIndexWithHEAD(
+            change.uri.fsPath
+          );
+          parsed.push(fileDiff);
+          break;
+      }
+    }
 
-		if (
-			model !== "llama-3.1-70b-versatile" &&
-			model !== "llama-3.1-8b-instant"
-		) {
-			vscode.window.showErrorMessage("Completion model is not set!");
-			return;
-		}
+    if (model !== "llama-3.3-70b-specdec" && model !== "llama-3.2-1b-preview") {
+      vscode.window.showErrorMessage("Completion model is not set!");
+      return;
+    }
 
-		if (!prompt || prompt.length < 10) {
-			vscode.window.showErrorMessage("Prompt is too short!");
-			return;
-		}
+    if (!prompt || prompt.length < 10) {
+      vscode.window.showErrorMessage("Prompt is too short!");
+      return;
+    }
 
-		await turboCompletion({
-			opts: {
-				messages: [
-					{
-						role: "system",
-						content: prompt,
-					},
-					{
-						role: "user",
-						content: `Git diff info in triple quotes:\n'''\n${parsed.join("\n")}\n\n${deleted.join("\n")}\n\n${renamed.join("\n")}\n'''\n`,
-					},
-				],
-				model,
-				max_tokens: 1024,
-				stream: true,
-			},
-			apiKey: openAiKey,
-			onText: (text) => (currentRepo.inputBox.value = text),
-			cancelToken,
-		});
-	} catch (error: any) {
-		vscode.window.showErrorMessage(error.toString());
-	}
+    await turboCompletion({
+      opts: {
+        messages: [
+          {
+            role: "system",
+            content: prompt,
+          },
+          {
+            role: "user",
+            content: `Git diff info in triple quotes:\n'''\n${parsed.join(
+              "\n"
+            )}\n\n${deleted.join("\n")}\n\n${renamed.join("\n")}\n'''\n`,
+          },
+        ],
+        model,
+        max_tokens: 1024,
+        stream: true,
+      },
+      apiKey: openAiKey,
+      onText: (text) => (currentRepo.inputBox.value = text),
+      cancelToken,
+    });
+  } catch (error: any) {
+    vscode.window.showErrorMessage(error.toString());
+  }
 };
 
 type TurboCompletion = (props: {
-	opts: {
-		messages: Array<{ role: "user" | "assistant" | "system"; content: string }>;
-		model: "llama-3.1-70b-versatile" | "llama-3.1-8b-instant";
-		max_tokens: number;
-		stream: boolean;
-	};
-	apiKey: string;
-	onText: (text: string) => void;
-	cancelToken: vscode.CancellationToken;
+  opts: {
+    messages: Array<{ role: "user" | "assistant" | "system"; content: string }>;
+    model: "llama-3.3-70b-specdec" | "llama-3.2-1b-preview";
+    max_tokens: number;
+    stream: boolean;
+  };
+  apiKey: string;
+  onText: (text: string) => void;
+  cancelToken: vscode.CancellationToken;
 }) => Promise<void | string>;
 
 /**
@@ -199,86 +198,86 @@ type TurboCompletion = (props: {
  * @returns {Promise<void | string>}
  */
 const turboCompletion: TurboCompletion = ({
-	opts,
-	apiKey,
-	onText,
-	cancelToken,
+  opts,
+  apiKey,
+  onText,
+  cancelToken,
 }) => {
-	return new Promise((resolve, reject) => {
-		const options = {
-			method: "POST",
-			hostname: "api.groq.com",
-			path: "/openai/v1/chat/completions",
-			headers: {
-				Authorization: `Bearer ${apiKey}`,
-				"Content-Type": "application/json",
-			},
-		};
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: "POST",
+      hostname: "api.groq.com",
+      path: "/openai/v1/chat/completions",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+    };
 
-		const req = request(options, (res) => {
-			const decoder = new TextDecoder("utf8");
+    const req = request(options, (res) => {
+      const decoder = new TextDecoder("utf8");
 
-			if (res.statusCode !== 200) {
-				let errorData = "";
-				res.on("data", (chunk) => {
-					errorData += decoder.decode(chunk);
-				});
-				res.on("end", () => {
-					try {
-						const errorObj = JSON.parse(errorData || "{}");
-						reject(
-							`OpenAI: ${res.statusCode} - ${errorObj.error?.code || "unknown"}`,
-						);
-					} catch (e) {
-						reject(`OpenAI: ${res.statusCode} - Error parsing error response`);
-					}
-				});
-				return;
-			}
+      if (res.statusCode !== 200) {
+        let errorData = "";
+        res.on("data", (chunk) => {
+          errorData += decoder.decode(chunk);
+        });
+        res.on("end", () => {
+          try {
+            const errorObj = JSON.parse(errorData || "{}");
+            reject(
+              `OpenAI: ${res.statusCode} - ${errorObj.error?.code || "unknown"}`
+            );
+          } catch (e) {
+            reject(`OpenAI: ${res.statusCode} - Error parsing error response`);
+          }
+        });
+        return;
+      }
 
-			let fullText = "";
-			let buffer = "";
+      let fullText = "";
+      let buffer = "";
 
-			res.on("data", (chunk) => {
-				buffer += decoder.decode(chunk);
-				let eolIndex;
-				while ((eolIndex = buffer.indexOf("\n")) >= 0) {
-					const line = buffer.substring(0, eolIndex).trim();
-					buffer = buffer.substring(eolIndex + 1);
-					if (line.startsWith("data:")) {
-						if (line === "data: [DONE]") {
-							resolve(fullText);
-						}
-						try {
-							const data = JSON.parse(line.substring(5));
-							const content = data.choices[0].delta.content;
-							if (content) {
-								fullText += content;
-								onText(fullText);
-							}
-						} catch (e: any) {
-							reject("Error parsing SSE data: " + e.message);
-						}
-					}
-				}
-			});
+      res.on("data", (chunk) => {
+        buffer += decoder.decode(chunk);
+        let eolIndex;
+        while ((eolIndex = buffer.indexOf("\n")) >= 0) {
+          const line = buffer.substring(0, eolIndex).trim();
+          buffer = buffer.substring(eolIndex + 1);
+          if (line.startsWith("data:")) {
+            if (line === "data: [DONE]") {
+              resolve(fullText);
+            }
+            try {
+              const data = JSON.parse(line.substring(5));
+              const content = data.choices[0].delta.content;
+              if (content) {
+                fullText += content;
+                onText(fullText);
+              }
+            } catch (e: any) {
+              reject("Error parsing SSE data: " + e.message);
+            }
+          }
+        }
+      });
 
-			res.on("end", () => {
-				resolve(fullText);
-			});
-		});
+      res.on("end", () => {
+        resolve(fullText);
+      });
+    });
 
-		req.on("error", (error) => {
-			reject(error);
-		});
+    req.on("error", (error) => {
+      reject(error);
+    });
 
-		req.write(JSON.stringify(opts));
+    req.write(JSON.stringify(opts));
 
-		req.end();
+    req.end();
 
-		cancelToken.onCancellationRequested(() => {
-			req.destroy();
-			resolve();
-		});
-	});
+    cancelToken.onCancellationRequested(() => {
+      req.destroy();
+      resolve();
+    });
+  });
 };
